0ce54cfd3762049b735201203a9e47d7131761dd
Blue Rogue <info@jozsefsimco.com>
2025-02-11 22:34:08 +0100

    initial commit

---
+++ react-crash-course/src/components/PostsList.jsx
@@ -0,0 +1,13 @@
+import Post from './Post';
+
+function PostsList() {
+    return (
+
+    <ul>
+        <Post author="Maximilian" body="React.js is awesome!"/>
+        <Post author="Manuel" body="Such a beautiful day!"/>
+    </ul>
+    );
+}
+
+export default PostsList;
